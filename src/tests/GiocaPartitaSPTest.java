4a2dbea291226533fbe12fb07c13a2fe10c992f5
gerof7 <gerog535@gmail.com>
2025-05-08 02:43:08 +0200

    aggiunta classe di test iterazione 2 + aggiornamento docx

---
+++ src/tests/GiocaPartitaSPTest.java
@@ -0,0 +1,40 @@
+package tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import GiocoDellOca.Pedina;
+import GiocoDellOca.Regola;
+import GiocoDellOca.Scenario;
+import GiocoDellOca.Tabellone;
+import GiocoDellOca.TipologiaRegolaEnum;
+
+class GiocaPartitaSPTest {
+
+	@Test
+	void pedinaTest() {
+		var pedina = new Pedina();
+		pedina.Muovi(4, 63);
+		assertEquals(pedina.getPosizione(), 5);
+		pedina.Muovi(59, 63);
+		assertEquals(pedina.getPosizione(), 62);		
+	}
+	
+	@Test
+	void tabelloneTest() {
+		Regola regola1 = new Regola("numero_caselle", "Seleziona il numero delle caselle di cui sarà composto il tabellone di gioco", "63", TipologiaRegolaEnum.NumeroCaselle);
+		Regola regola2 = new Regola("numero_dadi", "Seleziona il numero di dadi con cui vuoi giocare","2", TipologiaRegolaEnum.NumeroDadi);
+		List<Regola> regole = new ArrayList<Regola>();
+		regole.add(regola1);
+		regole.add(regola2);
+		var scenario = new Scenario("SCN-TEST", "Scenario Test", "", "", "", "", "", "");
+		
+		var tabellone = new Tabellone(regole, scenario);
+		assertNotNull(tabellone);	
+	}
+
+}
